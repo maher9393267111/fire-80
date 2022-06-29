589c0e3fb0bf386949315f35fdd5d9c500591ce1
maher <gomemahero.com>
2022-06-30 02:27:12 +0300

    maher 2

---
+++ mahercomp/maher1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Maher1 = () => {
+    return (
+        <div>
+            <h1>maher page1</h1>
+        </div>
+    );
+}
+
+export default Maher1;
---
+++ mahercomp/maher2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Maher2 = () => {
+    return (
+        <div>
+            <h1>maher page2</h1>
+        </div>
+    );
+}
+
+export default Maher2;
